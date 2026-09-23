5f542ee8d3e1357ffc15f1996486d43c12dc5339
MABO@MABO <mabobeatz@gmail.com>
2026-09-22 21:57:44 -0300

    Corrige linha duplicada nos servicos

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -226,7 +226,7 @@ function Index() {
 
             <ul className="mt-14 grid gap-px overflow-hidden rounded border border-border bg-border sm:grid-cols-2">
   {services.filter((service) => ["Manutenção", "Remoção"].includes(service.name)).map((service, i) => (
-              {services.map((service, i) => (
+             
                 <Reveal
                   as="li"
                   key={service.name}
